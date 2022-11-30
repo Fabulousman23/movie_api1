29c073c324401611f650d6078f9ad32aea43913a
Fabulousman23 <andrew.makiev@gmail.com>
2022-11-30 15:52:47 -0500

    edit

--- index.js
+++ index.js
@@ -32,9 +32,7 @@ let allowedOrigins = [
   "https://my-movie-app1234.herokuapp.com",
   "https://the-great-fabulousman23-site.netlify.app",
   "http://localhost:4200",
-  "https://fabulousman23.github.io",
-
-
+  "https://fabulousman23.github.io"
 ];
 
 app.use(
